625da38ee3f898faff4c8880f5546c33050b0711
GIT ddeveloperr <ddeveloperr@gmail.com>
2015-06-19 20:14:29 +0200

    Lab 1

---
+++ 1/script.jsx
@@ -0,0 +1,21 @@
+// Create first component and increment button 
+
+var Button = React.createClass({
+  getInitialState: function() {
+    return {counter: 0};
+  },
+  
+  handleClick: function() {
+    this.setState({counter: this.state.counter+1});
+  },
+  
+  
+  render: function() {
+  return (
+    <button onClick= {this.handleClick}>{this.state.counter}</button>
+    )  
+  } 
+  
+})
+
+React.render(<Button/>, document.getElementById("root"))
